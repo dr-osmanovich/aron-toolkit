9b26eb810de1ed70d93bb49a2f7e08b8da8f18cd
efoxmil <efox.mil@gmail.com>
2021-07-27 04:33:37 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from jinja2 import Environment, FileSystemLoader
+from pprint import pprint
+import csv
+
+SOURCE = 'sources/707n.csv'
+
+def get_data():
+    with open(SOURCE) as csvfile:
+        reader= csv.reader(csvfile, delimiter=';')
+        data = []
+        for row in reader:
+            item = {'ord': '', 'dest': []}
+            item['ord'] = row[0]
+            item['dest'] = [i for i in row[1:] if i]
+            item['dest'] = sorted(item['dest'])
+            data.append(item)
+    return data
+
+
+
+if __name__ == '__main__':
+    pprint(get_data())
